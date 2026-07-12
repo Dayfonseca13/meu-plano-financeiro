4e798d94573042983792073cde58b8992deea1d6
Dayfonseca13 <subliarte10@gmail.com>
2026-07-12 18:22:45 -0300

    fix: remove .ts extension from internal import in supabaseDb.ts

--- src/db/supabaseDb.ts
+++ src/db/supabaseDb.ts
@@ -88,7 +88,7 @@ async function handleSupabaseResult<T>(promise: PromiseLike<{ data: T | null; er
 }
 
 // Default fallback categories
-import { defaultCategories } from './constants.ts';
+import { defaultCategories } from './constants';
 
 export const supabaseDbManager = {
   // 1. USER METHODS
