aaacab7e71d91a81e6cbc1260fe4ff612622f0eb
Dayfonseca13 <subliarte10@gmail.com>
2026-07-12 20:40:36 +0000

    chore: remove script files

--- update_supabase.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-let content = fs.readFileSync('src/db/supabaseDb.ts', 'utf8');
-
-// We want to replace everything down to `let supabaseClient: any = null;`
-// Wait, it's easier to use sed or just replace the top part.
